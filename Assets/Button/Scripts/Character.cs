8cdea833a2a58b7060c441cc5aa257e8a6a5a6ae
Marc-Anderson  Gilles <hornil6748@gmail.com>
2022-11-27 19:18:40 -0500

    ajout script popups pour les damages numbers

---
+++ Assets/Button/Scripts/Character.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    //public int maxHealth = 100;
+    //public HealthBar healthbar;
+    //public GameObject damageText;
+    
+    //void Start()
+    //{
+    //    curHealth = maxHealth;
+    //}
+
+    //public void TakeDamage(int damage)
+    //{
+    //    curHealth -= damage;
+    //    healthbar.UpdateHealth(float)curHealth/ (float)maxHealth;
+    //}
+}
