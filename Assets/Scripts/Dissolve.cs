3131ab1cbb1d28d1992300619881c1ccdd625a05
VincentGelinas28 <75141000+VincentGelinas28@users.noreply.github.com>
2022-12-09 13:02:35 -0500

    Ajout de quelques effets

--- Assets/Scripts/Dissolve.cs
+++ Assets/Scripts/Dissolve.cs
@@ -8,13 +8,13 @@ public class Dissolve : MonoBehaviour
     public string PropertyName;
     public float LowValue = 0;
     public float HighValue = 5;
-    private float DissolveSpeed = 0.01f;
+    private float DissolveSpeed = 0.03f;
     private float MixValue = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MixValue = 0;
     }
 
     void Update()
